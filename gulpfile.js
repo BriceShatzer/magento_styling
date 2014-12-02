bf5eb26de45a3842f7721ff883043ffd77d6487d
Brice Shatzer <brice.shatzer@gmail.com>
2014-12-02 16:48:22 -0600

    Minor clean up

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,7 @@ var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var connect = require('gulp-connect');
 
-gulp.task('connect', function() {
-  
-});
-
 gulp.task('default', function() {
-  // place code for your default task here
-    //gulp.src('*.scss')
     gulp.src('./style.scss')
         .pipe(sass())
         .pipe(minifyCSS({
